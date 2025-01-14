21f79552484357353c9d183944f455bf32c37631
ruhiii19 <ruhii.panjwani19@gmail.com>
2025-01-14 11:47:20 +0530

    fetch from server

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,24 @@
-export default function Home() {
-  return <div className="text-2xl">Landing Page</div>;
+async function getData() {
+  const res = await fetch("https://dummyjson.com/products", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) throw new Error("Failed to fetch data");
+
+  return res.json();
+}
+
+export default async function Home() {
+  const data = await getData();
+  console.log("data", data);
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {data.products.length > 0 &&
+        data.products.map((product: any) => (
+          <div key={product.id}>
+            <p>{product.title}</p>
+          </div>
+        ))}
+    </main>
+  );
 }
